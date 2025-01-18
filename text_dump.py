cbf31c6c46e09176b830247328d4c6b631f0ab86
Aqil F <aqilfaizal2004@gmail.com>
2025-01-18 13:06:30 -0800

    colorified the output

--- text_dump.py
+++ text_dump.py
@@ -1,10 +1,15 @@
 from gitingest import ingest
-
+from colorama import Fore
 # summary, tree, content = ingest("path/to/directory")
 
-# or from URL
+# or from URL -> input a test repo URL
 summary, tree, content = ingest("https://github.com/aqil18/fully-self-driving-Pi")
 
-print(summary)
-print(tree)
-print(content)
+print(Fore.YELLOW + "\n\n\n SUMMARY: ")
+print(Fore.WHITE + summary)
+
+print(Fore.YELLOW + "\n\n\n TREE: ")
+print(Fore.WHITE + tree)
+
+print(Fore.YELLOW + "\n\n\n CONTENT: ")
+print(Fore.WHITE + content)
