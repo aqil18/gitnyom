691cc352fb3fc329954ab2d98cca9123839afd4e
Aqil F <aqilfaizal2004@gmail.com>
2025-01-19 10:48:15 -0800

    Fixed sizing

--- front-end/src/components/Anatomy.tsx
+++ front-end/src/components/Anatomy.tsx
@@ -2,8 +2,14 @@ import React, { useEffect } from 'react';
 
 const Anatomy: React.FC = () => {
   return (
-    <div>
-        <iframe src="http://localhost:8080" width="100%" height="100%" title="3D File Tree" />
+    <div style={{ width: '100%', height: '100%', position: 'relative' }}>
+      <iframe 
+        src="http://localhost:8080" 
+        width="100%" 
+        height="100%" 
+        title="3D File Tree" 
+        style={{ position: 'absolute', top: 0, left: 0, border: 'none' }}
+      />
     </div>
   );
 };
