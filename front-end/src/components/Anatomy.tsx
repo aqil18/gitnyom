7b231f7923082cb7997e0d48ab544790003a8af8
Aqil F <aqilfaizal2004@gmail.com>
2025-01-19 10:40:43 -0800

    updated feature name

--- front-end/src/components/Anatomy.tsx
+++ front-end/src/components/Anatomy.tsx
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react';
 const Anatomy: React.FC = () => {
   return (
     <div>
-        <iframe src="http://localhost:8080" width="100%" height="500px" title="Embedded Site" />
+        <iframe src="http://localhost:8080" width="100%" height="100%" title="3D File Tree" />
     </div>
   );
 };
