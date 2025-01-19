52eaf22df571da7058f03601f8670056ed3c4fcd
Minh Au <auduongminh2004@gmail.com>
2025-01-19 08:18:55 -0800

    WIP - build structure

--- backend/main.py
+++ backend/main.py
@@ -15,10 +15,10 @@ app = FastAPI()
 async def root():
     contributions = utils.contributions.get_commit_activity('https://github.com/supabase/supabase')
     response = utils.issues.get_issues('https://github.com/supabase/supabase')
-    summary, tree, content = await ingest('https://github.com/supabase/supabase')
-    utils.text_dump.summary(summary, tree, content)
-    resume_summary = utils.resume.generate_description(summary + tree + content)
-    return {'contributions': contributions, 'response': response, 'resume': resume_summary}
+    # summary, tree, content = ingest('https://github.com/supabase/supabase')
+    # utils.text_dump.summary(summary, tree, content)
+    # resume_summary = utils.resume.generate_description(summary + tree + content)
+    return {'contributions': contributions, 'issues': response, 'file_structure': resume_summary}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
